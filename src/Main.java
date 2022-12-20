29bc472264ebfaaa70415d665a21564c7f77dd7b
Brustik <minusminusov@gmail.com>
2022-12-20 23:43:58 +0300

    Methods ДЗ2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-//        task2();
+        task2();
 //        task3();
 
     }
@@ -18,7 +18,7 @@ public class Main {
         }
 
     }
-
+    
     public static boolean isYearLeap(int year) {
         boolean isYearLeap = false;
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {  //сгуглил, у самого код внизу:)
@@ -41,6 +41,39 @@ public class Main {
     public static void task2() {
         System.out.println();
         System.out.println("Задача 2");
+
+        int clientOS = 1;
+        int clientDeviceYear = 2022;
+        int checkDevice = checkClientDevice(clientOS, clientDeviceYear);
+        if (checkDevice == 1) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if (checkDevice == 2) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (checkDevice == 3) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        }
+    }
+    public static int checkClientDevice(int clientOS, int clientDeviceYear) {
+        int checkClientDevice;
+        int currentYear = 2022;
+        if (clientOS == 0 && clientDeviceYear == currentYear) {
+            checkClientDevice = 1;
+        } else if (clientOS == 0) {
+            checkClientDevice = 2;
+        } else if (clientDeviceYear == currentYear) {
+            checkClientDevice = 3;
+        } else {
+            checkClientDevice = 4;
+        }
+        return checkClientDevice;
+
+    }
+
+    public static void task3() {
+        System.out.println();
+        System.out.println("Задача 3");
 
     }
 }
